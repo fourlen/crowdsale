b732a0350adb7c3aec6e312bc13d6f28da9d4d64
fourlen <oborin.2013@mail.ru>
2022-06-24 13:15:16 +0300

    added interface

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -2,57 +2,58 @@
 
 pragma solidity 0.8.15;
 
-
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "../contracts/Stake.sol";
-
-
+import "../contracts/interfaces/IStake.sol";
 
 contract Crowdsale is Ownable, ReentrancyGuard {
-
     event crowdsaleStarted();
     event crowdsaleFinished();
-    event tokenPurchased(address buyer,uint amount);
-    event tokenClaimed(address user, uint amount);
-    event ownerClaimed(uint saleTokenAmount, uint paymentTokenAmount);
+    event tokenPurchased(address buyer, uint256 amount);
+    event tokenClaimed(address user, uint256 amount);
+    event ownerClaimed(uint256 saleTokenAmount, uint256 paymentTokenAmount);
 
-    IUniswapV2Router02 immutable public router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); //uniswap router address
+    IUniswapV2Router02 public immutable router =
+        IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); //uniswap router address
     IERC20Metadata public saleToken;
     IERC20Metadata public paymentToken;
-    Stake public stake;
+    IStake public stake;
 
-
-    uint public saleTokenAmount;
-    uint public dexTokenPercent;
-    uint public price; //wei of paymentToken for 1 sale token
-    uint public tokenSold;
+    uint256 public saleTokenAmount;
+    uint256 public dexTokenPercent;
+    uint256 public price; //wei of paymentToken for 1 sale token
+    uint256 public tokenSold;
     bool public saleStarted;
     bool public saleFinished;
 
-    mapping(address => uint) userPurchasedTokens;
-    mapping(Stake.Levels => uint) levelPoolAccess; //percents (for exmaple Iron level can buy only 5% of pool)
+    mapping(address => uint256) userPurchasedTokens;
+    mapping(IStake.Levels => uint256) levelPoolAccess; //percents (for exmaple Iron level can buy only 5% of pool)
 
-    constructor(IERC20Metadata _saleToken,
-                IERC20Metadata _paymentToken,
-                Stake _stake,
-                uint _saleTokenAmount,
-                uint _dexTokenPercent,
-                uint _price,
-                uint[5] memory _levelPoolPercent //from platinum to iron
-                ) {
-        require(address(_saleToken) != address(0) && address(_paymentToken) != address(0), "Token addressess can't be 0");
-        require(_saleTokenAmount != 0, "Token amount can't be 0"); //хз ставить проверку на dexTokenPerecent или нет, по идее мы же можем 
+    constructor(
+        IERC20Metadata _saleToken,
+        IERC20Metadata _paymentToken,
+        IStake _stake,
+        uint256 _saleTokenAmount,
+        uint256 _dexTokenPercent,
+        uint256 _price,
+        uint256[5] memory _levelPoolPercent //from platinum to iron
+    ) {
+        require(
+            address(_saleToken) != address(0) &&
+                address(_paymentToken) != address(0),
+            "Token addressess can't be 0"
+        );
+        require(_saleTokenAmount != 0, "Token amount can't be 0"); //хз ставить проверку на dexTokenPerecent или нет, по идее мы же можем
         //мы же можем захеть чтобы ни одного токена не пошло на декс или, наоборот, в 3 раза больше токенов пошло на декс
         require(_price != 0, "Price can't be 0");
         require(address(_stake) != address(0), "Stake address can't be 0");
-        uint percentSum = 0;
+        uint256 percentSum = 0;
         for (uint8 i = 0; i < 5; i++) {
             percentSum += _levelPoolPercent[i];
-            levelPoolAccess[Stake.Levels(i)] = _levelPoolPercent[i];
+            levelPoolAccess[IStake.Levels(i)] = _levelPoolPercent[i];
         }
         //хз нужно ли ставить проверку на то, что процент с повышением уровня должен повышаться, мало ли что придет в голову тому,
         //кто будет пользоваться этим контрактом, в этом плане у него больше свободы
@@ -69,33 +70,48 @@ contract Crowdsale is Ownable, ReentrancyGuard {
     //presale(0         0)          sale(1          0)      finished(1      1)
     function startCrowdsale() external onlyOwner {
         require(!saleStarted && !saleFinished, "Sale is already active");
-        require(saleToken.balanceOf(address(this)) >= saleTokenAmount * (1 + dexTokenPercent / 100), "insufficient balance of token for sale");
+        require(
+            saleToken.balanceOf(address(this)) >=
+                saleTokenAmount * (1 + dexTokenPercent / 100),
+            "insufficient balance of token for sale"
+        );
         saleStarted = true;
         emit crowdsaleStarted();
     }
 
     function finishCrowdsale() external onlyOwner {
-        require(saleStarted && !saleFinished, "Sale is already finished or hasn't started");
-        uint saleTokensToDex = tokenSold * dexTokenPercent / 100;
-        uint paymentTokenToDex = saleTokensToDex * price;
+        require(
+            saleStarted && !saleFinished,
+            "Sale is already finished or hasn't started"
+        );
+        uint256 saleTokensToDex = (tokenSold * dexTokenPercent) / 100;
+        uint256 paymentTokenToDex = saleTokensToDex * price;
         saleFinished = false;
-        router.addLiquidity(address(saleToken),
-                            address(paymentToken),
-                            saleTokensToDex,
-                            paymentTokenToDex, 
-                            saleTokensToDex, 
-                            paymentTokenToDex, 
-                            _msgSender(),
-                            block.timestamp + 1800); //30 min
+        router.addLiquidity(
+            address(saleToken),
+            address(paymentToken),
+            saleTokensToDex,
+            paymentTokenToDex,
+            saleTokensToDex,
+            paymentTokenToDex,
+            _msgSender(),
+            block.timestamp + 1800
+        ); //30 min
         emit crowdsaleFinished();
     }
 
     //amount in wei
-    function buy(uint _amount) external nonReentrant {
+    function buy(uint256 _amount) external nonReentrant {
         require(_amount <= saleTokenAmount - tokenSold, "Amount too big"); //not saleToken.balanceOf(address(this)) because giveaway will be after addLigiuidity
         tokenSold += _amount;
         address sender = _msgSender();
-        require(userPurchasedTokens[sender] + _amount <= saleTokenAmount * levelPoolAccess[stake.getUserLevel(sender)] / 100, "Amount too big for your level");
+        require(
+            userPurchasedTokens[sender] + _amount <=
+                (saleTokenAmount *
+                    levelPoolAccess[stake.getUserLevel(sender)]) /
+                    100,
+            "Amount too big for your level"
+        );
         userPurchasedTokens[sender] += _amount;
         SafeERC20.safeTransferFrom(
             paymentToken,
@@ -109,21 +125,17 @@ contract Crowdsale is Ownable, ReentrancyGuard {
     function claim() external nonReentrant {
         require(saleStarted && saleFinished, "Can claim only after finish");
         address sender = _msgSender();
-        uint balance = userPurchasedTokens[sender];
+        uint256 balance = userPurchasedTokens[sender];
         require(balance != 0, "Can't claim 0 tokens");
         userPurchasedTokens[sender] = 0;
-        SafeERC20.safeTransfer(
-            saleToken,
-            sender,
-            balance
-        );
+        SafeERC20.safeTransfer(saleToken, sender, balance);
         emit tokenClaimed(sender, balance);
     }
 
     function claimForOwner() external onlyOwner {
         require(saleStarted && saleFinished, "Can claim only after finish");
-        uint saleTokenBalance = saleToken.balanceOf(address(this));
-        uint paymentTokenBalance = paymentToken.balanceOf(address(this));
+        uint256 saleTokenBalance = saleToken.balanceOf(address(this));
+        uint256 paymentTokenBalance = paymentToken.balanceOf(address(this));
         address sender = _msgSender();
         SafeERC20.safeTransfer(saleToken, sender, saleTokenBalance - tokenSold);
         SafeERC20.safeTransfer(paymentToken, sender, paymentTokenBalance);
--- test/crowdsale-test.js
+++ test/crowdsale-test.js
@@ -1,11 +1,14 @@
 const { expect } = require("chai");
-const { BigNumber } = require("ethers");
+const { BigNumber, utils } = require("ethers");
 const { ethers } = require("hardhat");
 const { time } = require('@openzeppelin/test-helpers');
 
-const one_token = BigNumber.from("1e18");
-testERC20 = null;
+saleTokenTest = null;
+paymentTokenTest = null;
 stake = null;
+crowdsale = null;
+const hund_tokens = utils.parseEther("100");
+
 
 async function wait_year_and_collect_reward(expected_percent) {
   time.increase(365 * 86400);
@@ -26,16 +29,22 @@ async function wait_year_and_collect_reward(expected_percent) {
 describe("Deployment, transfering tokens, approvance and testing staking", function () {
   
 
-  it("Should deploy saleToken, paymentToken, stake and crowdsale contract", async function () {
+  it("Should deploy saleToken, paymentToken, stakeToken stake and crowdsale contract", async function () {
     const SaleTokenTest = await ethers.getContractFactory("testSale");
-    saleTokenTest = await SaleTokenTest.deploy(100 * one_token); //emit 1000 tokens
+    saleTokenTest = await SaleTokenTest.deploy(utils.parseEther("1000")); //emit 1000 tokens
     await saleTokenTest.deployed();
 
     console.log(`saleTokenTest address: ${saleTokenTest.address}`);
 
     const PaymentTokenTest = await ethers.getContractFactory("testPayment");
-    paymentTokenTest = await PaymentTokenTest.deploy(100 * one_token); //emit 1000 tokens
+    paymentTokenTest = await PaymentTokenTest.deploy(utils.parseEther("1000")); //emit 1000 tokens
     await paymentTokenTest.deployed();
+
+    console.log(`paymentTokenTest address: ${paymentTokenTest.address}`);
+
+    const StakeTokenTest = await ethers.getContractFactory("testStake");
+    stakeTokenTest = await StakeTokenTest.deploy(utils.parseEther("1000")); //emit 1000 tokens
+    await stakeTokenTest.deployed();
 
     console.log(`paymentTokenTest address: ${paymentTokenTest.address}`);
     
@@ -46,10 +55,43 @@ describe("Deployment, transfering tokens, approvance and testing staking", funct
     console.log(`Stake address: ${stake.address}`);
 
     const Crowdsale = await ethers.getContractFactory("Crowdsale");
-    crowdsale = await Crowdsale.deploy();
+    crowdsale = await Crowdsale.deploy(saleTokenTest.address, paymentTokenTest.address, stake.address, 100, 30, utils.parseEther("2.0"), [40, 30, 15, 10, 5]);
     await crowdsale.deployed();
 
     console.log(`Crowdsale address: ${crowdsale.address}`);
+  });
+
+
+  it("Should transfer 100 payment tokens to Alice and Bob, saleTokens to crowdsale and make approve", async function () {
+    const signers = await ethers.getSigners();
+    alice = signers[1].address;
+    bob = signers[2].address;
+    await paymentTokenTest.transfer(alice, hund_tokens);
+    await paymentTokenTest.transfer(bob, hund_tokens);
+    await stakeTokenTest.transfer(alice, 100); //for staking
+    await stakeTokenTest.transfer(bob, 100000);  //for staking
+    expect(await paymentTokenTest.balanceOf(alice)).to.equal(hund_tokens);
+    expect(await paymentTokenTest.balanceOf(bob)).to.equal(hund_tokens);
+    await saleTokenTest.transfer(crowdsale.address, utils.parseEther("130"));
+    expect(await saleTokenTest.balanceOf(crowdsale.address)).to.equal(utils.parseEther("130"));
+    await paymentTokenTest.connect(signers[1]).approve(crowdsale.address, hund_tokens);
+    await paymentTokenTest.connect(signers[2]).approve(crowdsale.address, hund_tokens);
+    expect(await paymentTokenTest.allowance(alice, crowdsale.address)).to.equal(hund_tokens);
+    expect(await paymentTokenTest.allowance(bob, crowdsale.address)).to.equal(hund_tokens);
+    await stakeTokenTest.connect(signers[1]).approve(stake.address, 100);
+    await stakeTokenTest.connect(signers[2]).approve(stake.address, 100000);
+    expect(await stakeTokenTest.allowance(alice, stake.address)).to.equal(100);
+    expect(await stakeTokenTest.allowance(bob, stake.address)).to.equal(100000);
+  });
+
+  it("Bob should send 100000 wei to stake, Alice should send 100 wei so Alice and Bob could buy 50 saleTokens", async function () {
+    const signers = await ethers.getSigners();
+    alice = signers[1].address;
+    bob = signers[2].address;
+    await stake.connect(signers[1]).deposit(100);
+    expect(await stake.getUserLevel(alice)).to.equal(3);
+    await stake.connect(signers[2]).deposit(100000);
+    expect(await stake.getUserLevel(bob)).to.equal(0);
   });
 
 
